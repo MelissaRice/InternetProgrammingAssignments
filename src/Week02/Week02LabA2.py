07e7e95fe923f8892ecef008fb65abbea9211e9a
Melissa Rice <melissafmtek@gmail.com>
2011-01-21 22:42:17 -0800

    Cleaned up the second half of lab A for week 2.

--- src/Week02/Week02LabA2.py
+++ src/Week02/Week02LabA2.py
@@ -1,6 +1,6 @@
 '''
-Week2LabA2.py
-last revised: 17 January 2011
+Week02LabA2.py
+last revised: 21 January 2011
 author: Melissa Rice (UWNetID: mlrice)
 purpose: quick test of urllib2 library
          assignment: write a program which reads a text file
@@ -13,51 +13,47 @@ import urllib2
 urlsFile = open('C:/A/eclipse/projects/InternetProgrammingLabs/src/Week02/urls.txt')
 urls = urlsFile.readlines()
 j = 0
+print
 for url in urls:
     j += 1
     urlObject = urllib2.urlopen(url)
     metadata = urlObject.info()
     urlAddress = urlObject.geturl()
     content = urlObject.readlines()
-    print urlAddress
-    print metadata
     content = ''.join(content)
-    print content
     outFilename = "C:/A/eclipse/projects/InternetProgrammingLabs/src/Week02/url" + str(j) + ".html"
+    print "Saving contents of %s" % urlAddress
+    print "To file: %s " % outFilename 
+    print "Sanitized url: %s " % urllib2.quote(urlAddress)
+    print "Metadata for this file is: %s \n\n" % metadata
     outFile = open(outFilename,'w')
-    outFile.write(urlAddress)
-    outFile.write(str(metadata))
     outFile.write(content)
     outFile.close()
 
 
-urlString = 'http://briandorsey.info/uwpython/week01/email_vm.py'
-url = urllib2.urlopen(urlString)
-metadata = url.info()
-urlAddress = url.geturl()
-content = url.readlines()
-output = "The url object: %s " % url
-output += "\n===> url address (url.geturl()): %s " % urlAddress 
-output += "\n===> url metadata (url.info()): %s " % metadata
-output += "\n===> url content: "
-output += ''.join(content)
-print output
-outputFilename = 'C:/A/eclipse/projects/InternetProgrammingLabs/src/Week02/Week2LabA1-Output.txt'
-outFile = open(outputFilename,'w')
-outFile.write(output)
-outFile.close()
-
 ''' Output of this code is:
 
+Saving contents of http://briandorsey.info/uwpython/week01/email_vm.py
+To file: C:/A/eclipse/projects/InternetProgrammingLabs/src/Week02/url1.html 
+Sanitized url: http%3A//briandorsey.info/uwpython/week01/email_vm.py 
+Metadata for this file is: Server: nginx
+Date: Sat, 22 Jan 2011 06:38:45 GMT
+Content-Type: application/octet-stream
+Content-Length: 891
+Last-Modified: Wed, 12 Jan 2011 18:28:19 GMT
+Connection: close
+Accept-Ranges: bytes
+ 
+
+
+Saving contents of http://www.melissarice.info/index.html
+To file: C:/A/eclipse/projects/InternetProgrammingLabs/src/Week02/url2.html 
+Sanitized url: http%3A//www.melissarice.info/index.html 
+Metadata for this file is: Server: Sun-ONE-Web-Server/6.1
+Date: Sat, 22 Jan 2011 06:06:48 GMT
+Content-type: text/html;charset=ISO-8859-1
+Connection: close
+ 
 
 
 '''
-
-
-
-
-
-
-
-
-
